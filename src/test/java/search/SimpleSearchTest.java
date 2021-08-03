60953761ad6595d90978628689a93b53265b9fde
Popa Vasile <vpopa@upro.local>
2021-08-03 17:08:12 +0300

    update 1.3

--- src/test/java/search/SimpleSearchTest.java
+++ src/test/java/search/SimpleSearchTest.java
@@ -7,21 +7,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.*;
 import org.openqa.selenium.Keys;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import romaniaup.selenium.test.AppConfig;
 
 
-public class SimpleSearchTest {
+public class SimpleSearchTest extends TestBase {
     @Test 
     public void simpleSearchWithOneKeyword(){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\vpopa\\Desktop\\javaOnline\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         
-        driver.get("http://testfasttrackit.info/selenium-test/");
+        driver.get(AppConfig.getSiteUrl());
         
         driver.findElement(By.id("search")).sendKeys("vase"+ Keys.ENTER );
         
-        List<WebElement> productName = driver.findElements(By.cssSelector("h2.product-name a"));
+        List<WebElement> productNames = driver.findElements(By.cssSelector("h2.product-name a"));
         
-        System.out.println(productName.toString());
+        System.out.println("Am stocat "+productNames.size() + " produse.");
+        
+        for(WebElement productName : productNames){
+            assertThat("ceva nu e bine",productName.getText(),containsString("VASE"));
+        
+            
+        System.out.println("Test completed de mine");
+        //driver.close();
+        }
         //driver.findElement(By.xpath("//div[@class='product-info' and ./descendant::*[text()='Herald Glass Vase']]//button[@title='Add to Cart']")).click();
         
         
